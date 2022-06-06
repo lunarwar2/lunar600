ce801c14aa5f5b150895d346baa06a10d575dcf6
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-06-06 20:46:33 +0700

    Create enamratus.js

---
+++ enamratus.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c99c926cc0f90ea9dbbd1bff032f4aac',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://snailteasesimulator.com/c99c926cc0f90ea9dbbd1bff032f4aac/invoke.js"></scr' + 'ipt>');
